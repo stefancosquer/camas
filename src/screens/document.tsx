e5c685db45c816d8dcfdbebd370de1b883c76d99
Stefan <stefan.cosquer@gmail.com>
2023-03-14 00:10:45 +0100

    added document parsing

--- src/screens/document.tsx
+++ src/screens/document.tsx
@@ -3,6 +3,12 @@ import { useBackend } from "../backends/backend";
 import { useSite } from "../hooks/site";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { unified } from "unified";
+import parse from "remark-parse";
+import frontmatter from "remark-frontmatter";
+import html from "remark-html";
+import { visit } from "unist-util-visit";
+import { load } from "js-yaml";
 
 export const Document = () => {
   const { "*": path } = useParams();
@@ -10,18 +16,29 @@ export const Document = () => {
   const { loadFile } = useBackend(site);
   const [content, setContent] = useState<string>();
   useEffect(() => {
-    loadFile(path).then(setContent);
+    loadFile(path).then(async (content) => {
+      const { data, value } = await unified()
+        .use(parse)
+        .use(frontmatter, ["yaml"])
+        .use(() => (tree, file) => {
+          visit(tree, "yaml", (node): void => {
+            file.data = load(node.value) as Record<string, unknown>;
+          });
+        })
+        .use(html)
+        .process(content);
+      console.log(data);
+      setContent(value);
+    });
   }, [path]);
   return (
     <Box
       sx={{
+        p: 2,
         display: "flex",
-        height: "100vh",
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Typography>{JSON.stringify(content, null, 2)}</Typography>
+      <Typography dangerouslySetInnerHTML={{ __html: content }} />
     </Box>
   );
 };
